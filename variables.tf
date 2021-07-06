09f37ee0c4a2ba4ae9fc833fdf3e0fb1e7072d00
archanachandran <haiarcha@gmail.com>
2021-07-06 14:07:57 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "AWS_REGION_US_EAST" {    
+    default = "us-east-1"
+}
+
+variable "AWS_REGION_US_WEST" {    
+    default = "us-west-1"
+}
+
+variable "vpc-name" {
+    name = "us-east-1-vpc-app"
+}
+
